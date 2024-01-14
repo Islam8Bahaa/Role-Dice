f6f7da554c8053129a2a9ed1d1ee9e579f8cef4e
Islam8Bahaa <81769785+Islam8Bahaa@users.noreply.github.com>
2024-01-14 19:56:55 +0200

    Role Dice

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+
+const current0 = document.getElementById("current--0");
+const current1 = document.getElementById("current--1");
+
+const player0 = document.getElementById("player--0");
+const player1 = document.getElementById("player--1");
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0.textContent = 0;
+score1.textContent = 0;
+current0.textContent = 0;
+current1.textContent = 0;
+
+
+
+
+
